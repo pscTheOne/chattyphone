f149d716690957019b3f64df3411f06f69e4f0dd
Phil <psc@Phils-MacBook-Pro.local>
2024-07-15 17:19:01 +0100

    lala

--- controller.py
+++ controller.py
@@ -1,10 +1,10 @@
 # controller.py
 from keypad_controller import KeypadController
-from motion_sensor import MotionSensor
+#from motion_sensor import MotionSensor
 
 class Controller:
     def __init__(self, motion_sensor_pin=37, keypad_sleep_time=0.01, keypad_max_cycles=20, keypad_debounce_cycles=10):
-        self.motion_sensor = MotionSensor(motion_sensor_pin)
+#        self.motion_sensor = MotionSensor(motion_sensor_pin)
         self.keypad_controller = KeypadController(
             sleep_time=keypad_sleep_time,
             max_cycles=keypad_max_cycles,
@@ -12,11 +12,11 @@ class Controller:
         )
         self.keypad_input = ""
 
-    def detect_motion(self):
-        return self.motion_sensor.detect_motion()
+#    def detect_motion(self):
+#        return self.motion_sensor.detect_motion()
 
-    def get_last_motion_time(self):
-        return self.motion_sensor.get_last_motion_time()
+#    def get_last_motion_time(self):
+#        return self.motion_sensor.get_last_motion_time()
 
     def run_keypad(self):
         self.keypad_controller.run()
